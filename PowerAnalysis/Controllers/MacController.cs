385c0814416810cdfff5c943654e1eaeb9306f59
Mark Cheeseman <mcheesemanau@gmail.com>
2012-10-12 01:14:14 +1100

    Pass chart data to view from controller

--- PowerAnalysis/Controllers/MacController.cs
+++ PowerAnalysis/Controllers/MacController.cs
@@ -1,8 +1,23 @@
 ﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace PowerAnalysis.Controllers
 {
-	public class MacController: Controller
+	public class ChartItem
+	{
+		public int x { get; set; }
+		public int y { get; set; }
+		public string label { get; set; }
+	}
+
+	public class Chart
+	{
+		public ChartItem[] Items { get; set; }
+	}
+
+
+	public class MacController : Controller
 	{
 		public ActionResult Index()
 		{
@@ -11,9 +26,24 @@ namespace PowerAnalysis.Controllers
 
 		public ActionResult Set(string setName)
 		{
-			return View();
+			var chart = new Chart
+			            	{
+			            		Items = new[]
+			            		        	{
+			            		        		new ChartItem() {x = 10, y = -200, label = "theLabel"},
+			            		        		new ChartItem() {x = 20, y = -50, label = "theLabel"},
+			            		        		new ChartItem() {x = 30, y = 50, label = "theLabel"},
+			            		        		new ChartItem() {x = 40, y = 100, label = "item2"}
+			            		        	}
+			            	};
+			object serializedChart = JsonConvert.SerializeObject(chart,
+			                                                     Formatting.Indented,
+			                                                     new JsonSerializerSettings
+			                                                     	{
+			                                                     		ContractResolver =
+			                                                     			new CamelCasePropertyNamesContractResolver()
+			                                                     	});
+			return View(serializedChart);
 		}
-
-
 	}
 }
